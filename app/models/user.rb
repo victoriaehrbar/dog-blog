6286f00bf3a64c97aa30c42b9b66eaef987982de
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-22 19:50:28 -0600

    migration issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :updates
-    has_many :dogs, through :updates
+    has_many :dogs, through: :updates
 
     validates :email, uniqueness: true, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
     validates :name, presence: true
