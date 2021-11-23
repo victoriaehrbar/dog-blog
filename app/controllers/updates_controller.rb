1055604b6c63b8ef08e6ff2179cf65dfe27a7aff
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-23 18:30:38 -0500

    form

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,2 +1,8 @@
 class UpdatesController < ApplicationController
+    before_action :require_login , except: [:index, :show]
+
+    def new
+        @dog = Dog.find_by_id(params[:dog_id])
+        @update = @dog.updates.build
+    end
 end
