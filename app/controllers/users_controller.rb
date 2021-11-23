3b18dfbb0a4c1124aac289cd8b2cb39402454f18
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-22 21:04:26 -0600

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,16 @@ class UsersController < ApplicationController
     end
 
     def create
-        # @user = User.new(user_params)
-        # if @user.save
-        #     session[:user_id] = @user.id    #logs user in
-        #     redirect_to user_path(@user)
-        # else
-        #     render :new
-        # end
+        # byebug
+        @user = User.new(user_params)
+        if @user.save
+            #log user in
+            session[:user_id] = @user.id 
+            #redirect to show page
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def show
