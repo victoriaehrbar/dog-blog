699c7db466f0fb68476288ecc908ba6b8bb4431e
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-22 20:17:16 -0600

    gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :dogs, through: :updates
 
     validates :email, uniqueness: true, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
-    validates :name, presence: true
+    validates :username, presence: true
 
 
     has_secure_password
