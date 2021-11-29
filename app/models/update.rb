2e38dc90de17fcaec3e02c285ec496ea6375af58
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-29 12:15:16 -0600

    update model

--- app/models/update.rb
+++ app/models/update.rb
@@ -3,4 +3,6 @@ class Update < ApplicationRecord
   belongs_to :dog
 
   validates :content, :title, :training_score, presence: true
+
+  scope :high_training_score, -> {where("training_score > 6")}
 end
