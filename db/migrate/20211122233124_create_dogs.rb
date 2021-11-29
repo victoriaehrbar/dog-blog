b2d74c7f2815bf95e79d4798178a7856faa2aa09
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-29 10:41:39 -0600

    migration issue

--- db/migrate/20211122233124_create_dogs.rb
+++ db/migrate/20211122233124_create_dogs.rb
@@ -4,6 +4,7 @@ class CreateDogs < ActiveRecord::Migration[6.1]
       t.string :dog_name
       t.string :mood
       t.integer :weight
+      t.string :breed
       t.integer :breed_id
       t.integer :user_id
 
