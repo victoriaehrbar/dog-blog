a96221d50b1e44ca071ccf3e0623515e8f3dcdc7
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-29 12:14:15 -0600

    dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :breed
 
-  has_many :updates 
+  has_many :updates, dependent: :destroy
   has_many :users, through: :updates
 
   validates :dog_name, presence: true
