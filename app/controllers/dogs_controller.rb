c96efc50cadd373b1c5e6e5b28ab0f0a718b0aff
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-29 12:24:52 -0600

    dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -15,5 +15,28 @@ class DogsController < ApplicationController
         @dog = Dog.new
     end
 
-    
+    def create
+        @breeds = ["Golden Retriever", "German Shepherd", "Husky", "Collie", "Other"]
+        @dog = Dog.find_by(dog_name: params[:dog][:dog_name])
+            if @dog
+                flash[:message] = "This dog already exists, please write an update about this dog or add a new one"
+                redirect_to dogs_path
+            else
+                @dog = Dog.new(dog_params)
+                @dog.user_id = session[:user_id]
+                @dog.save
+                if !@dog.breed_id
+                    render :new
+                else
+                    redirect_to dog_path(@dog)
+                end
+            end
+        end
+
+        def edit
+        end
+
+        def update
+        end
+
 end
