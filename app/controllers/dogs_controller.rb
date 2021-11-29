e932f303a433b67dc63315e3cfd39abcd21b0cf1
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-28 19:42:52 -0600

    dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,9 @@
 class DogsController < ApplicationController
+    before_action :require_login, except: [:index, :show]
+    before_action :find_dog, only [:show, :edit, :update, :destroy]
+    before_action :current_user
+
+    def index
+        @dogs = Dog.all 
+    end
 end
